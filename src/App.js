4d12709151efd3adf7fbda2973b1f066ca842ae6
Shaun Swass <shaunswass@gmail.com>
2018-10-16 14:04:30 -0400

    structure setup, navbar component done, title component done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form  from './components/form'
+import Header  from './components/header'
+import Title  from './components/title'
+import Weather  from './components/weather'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <Title />
+            <div className="col-md-8">
+              <div className="row">
+                <div className="col-md-12">
+                  <Form />
+              <div className="row">
+                <div className="col-md-12">
+                  <Weather />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/form/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="Form">
+
+      </div>
+    );
+  }
+}
+
+export default Form;
